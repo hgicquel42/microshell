c8197181f45460392422e4d94dba1ff4baa933fa
hgicquel <hgicquel@student.42.fr>
2022-02-16 19:37:59 +0100

    fixes

--- microshell2.c
+++ microshell2.c
@@ -1,5 +1,5 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <stdbool.h>
 #include <string.h>
@@ -14,7 +14,7 @@ typedef struct s_cmd
 	int		fdo;
 }	t_cmd;
 
-typedef struct s_state
+typedef struct s_global
 {
 	int		argc;
 	char	**argv;
@@ -22,7 +22,7 @@ typedef struct s_state
 	int		cmdc;
 	t_cmd	*cmdv;
 	int		sts;
-}	t_state;
+}	t_global;
 
 int	ft_strlen(char *s)
 {
@@ -51,7 +51,7 @@ void	ft_copy(t_cmd *cmd, int j, char *s)
 		cmd->args[j] = s;
 }
 
-int	ft_parse(t_cmd *cmd, t_state *g, int i)
+int	ft_parse(t_cmd *cmd, t_global *g, int i)
 {
 	int	j;
 
@@ -61,39 +61,39 @@ int	ft_parse(t_cmd *cmd, t_state *g, int i)
 		if (!strcmp(g->argv[i], ";") || !strcmp(g->argv[i], "|"))
 		{
 			if (!j)
-				ft_copy(cmd, j++, g->argv[i++]);
+				ft_copy(cmd, j++, g->argv[i]);
 			break ;
 		}
-		ft_copy(cmd, j++, g->argv[i++]);
+		ft_copy(cmd, j++, g->argv[i]);
+		i++;
 	}
-	if (cmd)
-		cmd->args[j] = NULL;
+	ft_copy(cmd, j, NULL);
 	return (j);
 }
 
-int	ft_split(t_state *g)
+int	ft_split(t_global *g, t_cmd *cmdv)
 {
-	int		i;
-	int		l;
-	int		j;
+	int	i;
+	int	l;
+	int	j;
 
 	i = 0;
 	l = 0;
 	while (i < g->argc)
 	{
 		j = ft_parse(NULL, g, i);
-		if (j && !g->cmdv)
+		if (j && !cmdv)
 			l++;
-		if (j && g->cmdv)
+		if (j && cmdv)
 		{
-			g->cmdv[l].pipe[0] = 0;
-			g->cmdv[l].pipe[1] = 0;
-			g->cmdv[l].fdi = 0;
-			g->cmdv[l].fdo = 0;
-			g->cmdv[l].args = malloc((j + 1) * sizeof(char *));
-			if (!g->cmdv[l].args)
+			cmdv[l].pipe[0] = 0;
+			cmdv[l].pipe[1] = 0;
+			cmdv[l].fdi = 0;
+			cmdv[l].fdo = 0;
+			cmdv[l].args = malloc((j + 1) * sizeof(char *));
+			if (!cmdv[l].args)
 				return (l);
-			if (ft_parse(&g->cmdv[l], g, i) != j)
+			if (ft_parse(&cmdv[l], g, i) != j)
 				return (l);
 			l++;
 		}
@@ -102,10 +102,10 @@ int	ft_split(t_state *g)
 	return (l);
 }
 
-int	ft_exec(t_state *g, t_cmd *cmd)
+int	ft_exec(t_global *g, t_cmd *cmd)
 {
-	int		pid;
-	int		sts;
+	int	sts;
+	int	pid;
 
 	sts = 0;
 	pid = fork();
@@ -125,10 +125,19 @@ int	ft_exec(t_state *g, t_cmd *cmd)
 		waitpid(pid, &sts, 0);
 	if (WIFEXITED(sts))
 		g->sts = WEXITSTATUS(sts);
+	if (pid == -1)
+		ft_fatal();
+	if (pid == 0)
+	{
+		ft_error("error: cannot execute ");
+		ft_error(cmd->args[0]);
+		ft_error("\n");
+		exit(1);
+	}
 	return (pid);
 }
 
-void	ft_run(t_state *g)
+void	ft_run(t_global *g)
 {
 	int	i;
 
@@ -143,7 +152,7 @@ void	ft_run(t_state *g)
 		{
 			if (!g->cmdv[i].args[1])
 				g->sts = ft_error("error: cd: bad arguments\n");
-			else if (chdir(g->cmdv[i].args[1]) == -1)
+			else if (chdir(g->cmdv[i].args[0]) == -1)
 			{
 				ft_error("error: cd: cannot change directory to ");
 				ft_error(g->cmdv[i].args[1]);
@@ -157,30 +166,28 @@ void	ft_run(t_state *g)
 			{
 				if (pipe(g->cmdv[i + 2].pipe) == -1)
 					ft_fatal();
-				g->cmdv[i].fdo = g->cmdv[i + 2].pipe[1];
 				g->cmdv[i + 2].fdi = g->cmdv[i + 2].pipe[0];
+				g->cmdv[i].fdo = g->cmdv[i + 2].pipe[1];
 			}
 			ft_exec(g, &g->cmdv[i]);
 		}
-		free(g->cmdv[i++].args);
+		i++;
 	}
-	free(g->cmdv);
 }
 
 int	main(int argc, char **argv, char **envp)
 {
-	t_state	g;
+	t_global g;
 
 	g.argc = argc - 1;
 	g.argv = argv + 1;
 	g.envp = envp;
-	g.cmdv = NULL;
-	g.cmdc = ft_split(&g);
-	g.cmdv = malloc(g.cmdc * sizeof(t_cmd));
 	g.sts = 0;
+	g.cmdc = ft_split(&g, NULL);
+	g.cmdv = malloc(g.cmdc * sizeof(t_cmd));
 	if (!g.cmdv)
 		ft_fatal();
-	if (ft_split(&g) != g.cmdc)
+	if (ft_split(&g, g.cmdv) != g.cmdc)
 		ft_fatal();
 	ft_run(&g);
 	return (g.sts);
